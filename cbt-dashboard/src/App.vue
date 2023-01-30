177726d208723b546a0a8f7a793c1295eed38240
EsmeraldaKok <EKok@exigent-group.com>
2023-01-30 14:55:19 +0200

    app.vue update

--- cbt-dashboard/src/App.vue
+++ cbt-dashboard/src/App.vue
@@ -20,51 +20,16 @@ export default {
     components: { AddUser }
 }
 </script>
-
-<style lang="scss" scoped>
-  .user_select {
-            position: relative;
-            max-width: 266px;
-            width: 100%;
-            height: 34px;
-            background-color: white;
-            border: 1px solid black;
-            border-radius: 5px;
-            margin-bottom: 15px;
-            
-            .selected-value {
-                padding: 5px 5px;
-                text-align: left; 
-                cursor: pointer;
-                font-size: 1rem;
-                color: black;
-                font-weight: 700;
-            }
-
-            .options {
-                position: absolute;
-                top: calc(100% + 3px);
-                left: 0;
-                width: 100%;
-                max-height: 200px;
-                overflow-y: scroll;
-                background: linear-gradient(284deg, rgba(255, 255, 255, 0.986) 45%, rgba(223, 223, 223, 0.986) 92%);
-                border-radius: 5px;
-                overflow-y: hidden;
-
-                .option {
-                    color: black;
-                    padding: 5px;
-                    font-size: 1rem;
-                    font-weight: 300;
-                    cursor: pointer;
-                    
-
-                    &:hover {
-                        background-color: #00CC99;
-                        color: white;
-                    }
-                }
-            }
-        }
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,500;1,900&display=swap');
+    
+/* general  styles*/
+*, 
+*::after, 
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
 </style>
